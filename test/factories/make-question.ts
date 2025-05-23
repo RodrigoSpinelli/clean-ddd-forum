af0230ac4fea7bf4956a9832dd2d3f7028c6c39c
Rodrigo Spinelli <rodrigo.spinelli9@gmail.com>
2025-05-23 02:18:35 -0300

    feat: enhance makeQuestion factory to use faker for dynamic question content

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -3,14 +3,21 @@ import {
   QuestionProps,
 } from '@/domain/forum/enterprise/entities/question';
 import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { faker } from '@faker-js/faker';
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    title: 'Nova pergunta',
-    content: 'Conteúdo da pergunta',
-    authorId: new UniqueEntityID(),
-    ...override,
-  });
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID
+) {
+  const question = Question.create(
+    {
+      title: faker.lorem.sentence(),
+      content: faker.lorem.paragraph(),
+      authorId: new UniqueEntityID(),
+      ...override,
+    },
+    id
+  );
 
   return question;
 }
