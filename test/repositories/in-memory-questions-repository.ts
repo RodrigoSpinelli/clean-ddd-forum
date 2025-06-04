29bec3363b483569a62f7f0f5105003f602288bf
Rodrigo Spinelli <rodrigo.spinelli9@gmail.com>
2025-06-04 20:48:16 -0300

    feat: add save method to QuestionsRepository and implement in InMemoryQuestionsRepository

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -27,4 +27,12 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
 
     return question ?? null;
   }
+
+  async save(question: Question): Promise<void> {
+    const questionIndex = this.items.findIndex(
+      (item) => item.id === question.id
+    );
+
+    this.items[questionIndex] = question;
+  }
 }
